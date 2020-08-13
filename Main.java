64494849037fc60e5a241616d25b7b9ba4e70693
bzzuma24 <60631894+bzzuma24@users.noreply.github.com>
2020-08-13 08:08:08 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My favorite color is Blue.");
+		System.out.println("I am the middle child in my family.");
+		System.out.println("My favorite ice cream is Coffee and Coconut");
+		System.out.println("I am Ghanian, Ivorian,and Congolese.");
+		System.out.println("I have been playing rugby for 4 years.");
+	}
+}
